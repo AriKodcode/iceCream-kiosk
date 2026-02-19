45714519d327679307cbda13262e18591d7ed5c8
Ari Durlacher <arikodcode@gmail.com>
2026-02-19 15:05:27 +0200

    m

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,41 @@
-import 'Card.css';
+import './Card.css';
 import { useState } from 'react';
 
 export default function Card(props) {
   const [scoopsIce, setScoopsIce] = useState(0);
-  function minus() {
+  const [error, setError] = useState('');
+
+  function handleMinusClick() {
+    setError('');
     if (scoopsIce > 0) {
-      () => setScoopsIce(scoopsIce - 1);
+      setScoopsIce(scoopsIce - 1);
+    } else {
+      setError('error');
     }
   }
-  function plus() {
+  function handlePlusClick() {
+    setError('');
     if (scoopsIce < 5) {
-      () => setScoopsIce(scoopsIce + 1);
+      setScoopsIce(scoopsIce + 1);
+    } else {
+      setError('you cant order more then 5 scoop');
     }
   }
+
   return (
     <div className="card">
       <h1>{props.name}</h1>
       <div className="card-buttons">
-        <button onClick={minus}>-</button>
-        {scoopsIce}
-        <button onClick={plus}>+</button>
+        <div>
+          <button onClick={handleMinusClick}>-</button>
+        </div>
+        <p>{scoopsIce}</p>
+        <div>
+          <button onClick={handlePlusClick}>+</button>
+        </div>
       </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
     </div>
   );
 }
--- src/components/Flavor_list/Flavor_list.jsx
+++ src/components/Flavor_list/Flavor_list.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
-import 'Flavor_list.css';
+import './Flavor_list.css';
 import Card from '../Card/Card';
 import iceCream from '../../../public/data.json';
 
 export default function Flavor_list() {
   const [data, setdata] = useState(iceCream);
-  
+
   return (
-    <div>
-        {data.map((obj)=>{
-            return(<Card name={obj.name}  />)
-        })}
-      
+    <div className="flavors">
+      {data.map((obj) => {
+        if (!obj.isSoldOut) {
+          return <Card key={obj.name} name={obj.name} />;
+        }
+      })}
     </div>
   );
 }
